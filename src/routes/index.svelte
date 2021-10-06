f63d08182f5c7c4a4f0f3a323d1cb650b4964899
rudywaltz <david.barkoczi@gmail.com>
2021-10-06 22:51:58 +0200

    cleanup example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<script>
   /**
    * @type {import('@sveltejs/kit').Load}
    */
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
-import node from '@sveltejs/adapter-node';
-import sveltePreprocess from 'svelte-preprocess';
 const config = {
-	preprocess: sveltePreprocess(),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: node({ env: { port: process.env.PORT } })
+		target: '#svelte'
 	}
 };
 
